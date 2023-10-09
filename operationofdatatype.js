32254745a23b55ac7bb588b0ac87ae6d9adaf11a
Anurag Bhaik <101393504+Anuragbhaik@users.noreply.github.com>
2023-10-09 18:36:19 +0530

    Add files via upload

---
+++ operationofdatatype.js
@@ -0,0 +1,20 @@
+// let number = 22;
+// console.log(number);
+// console.log(typeof number);
+
+
+let string = "21" ;
+//console.log(string);
+//console.log(typeof string);
+
+
+let one ="22abc";
+//console.log(Number(one));
+//console.log(typeof one);
+
+
+
+let Anurag = "22abc";
+let number = Number(Anurag);
+console.log(number);
+console.log(typeof number);
